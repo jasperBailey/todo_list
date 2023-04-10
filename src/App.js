0ac4ee23b67055b82dbb51fba0ab7a70f15db6d9
user <user@users-MacBook-Pro.local>
2023-04-10 15:34:41 +0100

    added mvp and extensions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [toDoItems, setToDoItems] = useState([
+    {
+      name: "Buy shopping",
+      priority: "high",
+    },
+    {
+      name: "Clean bathroom",
+      priority: "low",
+    },
+    {
+      name: "Car's MOT",
+      priority: "high",
+    },
+  ]);
+
+  const [newItem, setNewItem] = useState("");
+  const [newItemPriority, setNewItemPriority] = useState("");
+
+  const handleNewItemChange = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const handleNewItemPriorityChange = (event) => {
+    setNewItemPriority(event.target.value);
+  };
+
+  const addNewItem = (event) => {
+    event.preventDefault();
+    const copyToDoItems = [...toDoItems];
+    copyToDoItems.push({
+      name: newItem,
+      priority: newItemPriority
+    });
+    setToDoItems(copyToDoItems);
+    setNewItem("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <ul>
+        {toDoItems.map((item) => {
+          return (
+            <li className={item.priority==='high' ? 'high-priority' : 'low-priority' }>
+              <h3>{item.name}</h3>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={addNewItem}>
+        <label htmlFor="new-item">Add a To Do: </label>
+
+        <input
+          type="text"
+          id="new-item"
+          value={newItem}
+          onChange={handleNewItemChange}
+        />
+
+        <label htmlFor="high-priority">High</label>
+        <input type="radio" id="high-priority" name="priority" value="high" onChange={handleNewItemPriorityChange}/>
+
+        <label htmlFor="low-priority">Low</label>
+        <input type="radio" id="low-priority" name="priority" value="low" onChange={handleNewItemPriorityChange}/>
+
+        <button>Add</button>
+      </form>
     </div>
   );
 }
